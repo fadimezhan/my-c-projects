07474b26d0a9feaaf216c65407b6d9b7c31848c7
fadimezhan <fadime_ozhan@hotmail.com>
2015-05-18 20:30:55 +0300

    PizzaSiparisi
    pizza-siparisi

--- pizza1/Properties/AssemblyInfo.cs
+++ pizza1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Şirket")]
+[assembly: AssemblyTitle("pizza1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Şirket")]
+[assembly: AssemblyProduct("pizza1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("574792c8-b593-449c-aa0a-3162e0259035")]
+[assembly: Guid("f202206a-7eac-46e1-995a-27a78e050fcf")]
 
 // Version information for an assembly consists of the following four values:
 //
