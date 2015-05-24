6b4708f35bad6c164bf1398f1076f4f78ce7d07e
fadimezhan <fadime_ozhan@hotmail.com>
2015-05-25 00:32:26 +0300

    sqlDeneme
    veri-ekler-siler-gunceller

---
+++ sqlDeneme/sqlDeneme/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace sqlDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ıdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ısimDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyisimDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numaraDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tblKisilerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.denemeDataSet = new sqlDeneme.denemeDataSet();
+            this.tblKisilerTableAdapter = new sqlDeneme.denemeDataSetTableAdapters.tblKisilerTableAdapter();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxAra = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.denemeDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıdDataGridViewTextBoxColumn,
+            this.ısimDataGridViewTextBoxColumn,
+            this.soyisimDataGridViewTextBoxColumn,
+            this.numaraDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblKisilerBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 272);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // ıdDataGridViewTextBoxColumn
+            // 
+            this.ıdDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.ıdDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.ıdDataGridViewTextBoxColumn.Name = "ıdDataGridViewTextBoxColumn";
+            this.ıdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // ısimDataGridViewTextBoxColumn
+            // 
+            this.ısimDataGridViewTextBoxColumn.DataPropertyName = "Isim";
+            this.ısimDataGridViewTextBoxColumn.HeaderText = "Isim";
+            this.ısimDataGridViewTextBoxColumn.Name = "ısimDataGridViewTextBoxColumn";
+            // 
+            // soyisimDataGridViewTextBoxColumn
+            // 
+            this.soyisimDataGridViewTextBoxColumn.DataPropertyName = "Soyisim";
+            this.soyisimDataGridViewTextBoxColumn.HeaderText = "Soyisim";
+            this.soyisimDataGridViewTextBoxColumn.Name = "soyisimDataGridViewTextBoxColumn";
+            // 
+            // numaraDataGridViewTextBoxColumn
+            // 
+            this.numaraDataGridViewTextBoxColumn.DataPropertyName = "Numara";
+            this.numaraDataGridViewTextBoxColumn.HeaderText = "Numara";
+            this.numaraDataGridViewTextBoxColumn.Name = "numaraDataGridViewTextBoxColumn";
+            // 
+            // tblKisilerBindingSource
+            // 
+            this.tblKisilerBindingSource.DataMember = "tblKisiler";
+            this.tblKisilerBindingSource.DataSource = this.denemeDataSet;
+            // 
+            // denemeDataSet
+            // 
+            this.denemeDataSet.DataSetName = "denemeDataSet";
+            this.denemeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblKisilerTableAdapter
+            // 
+            this.tblKisilerTableAdapter.ClearBeforeFill = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxAra
+            // 
+            this.textBoxAra.Location = new System.Drawing.Point(497, 42);
+            this.textBoxAra.Multiline = true;
+            this.textBoxAra.Name = "textBoxAra";
+            this.textBoxAra.Size = new System.Drawing.Size(161, 45);
+            this.textBoxAra.TabIndex = 3;
+            this.textBoxAra.TextChanged += new System.EventHandler(this.textBoxAra_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(513, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 63);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 354);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 354);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(349, 354);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(127, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "İsim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Soyisim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Numara";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 505);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxAra);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.denemeDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private denemeDataSet denemeDataSet;
+        private System.Windows.Forms.BindingSource tblKisilerBindingSource;
+        private denemeDataSetTableAdapters.tblKisilerTableAdapter tblKisilerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ısimDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyisimDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numaraDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxAra;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
